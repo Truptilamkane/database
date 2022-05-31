4e998b3a35e2be59e244dc7d24b308dd975d526c
Your Name <you@example.com>
2022-05-31 16:21:00 +0530

    first commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+Create database SampleDb1;
+
+create table empolyee (id int,name varchar(200),adress varchar(200),salary int);
+
+insert into empolyee values(1,'Vikash ','mumbai',100000);
+insert into empolyee values(2,'shubhangi ','pune',21000);
+insert into empolyee values(3,'pallavi ','satara',350000);
+insert into empolyee values(4,'arjun ','latur',800000);
+insert into empolyee values(5,'shivam','baglore',430000);
+
+select * from empolyee ;
