f70ad6091d5ca4f30f046beb7caa6d3880825d71
Your Name <you@example.com>
2022-05-31 15:59:29 +0530

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+
+create table sample(id int,name varchar(200));
+
+select *from sample;
+
+insert into sample values(1,'shree 1');
+insert into sample values(2,'shree 2');
+insert into sample values(3,'shree 3');
+
+delete from sample where id=1;
+
+update sample set name='om'where id=2;
+
+
+ 
